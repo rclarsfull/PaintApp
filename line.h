d111142645f09d9ff389f41a6b33578742cac36f
Lars <l.p.1999@live.de>
2023-05-17 15:32:26 +0200

    implemented Line header

--- line.h
+++ line.h
@@ -1,16 +1,20 @@
 #ifndef LINE_H
 #define LINE_H
 
+#include <QPoint>
 #include "GraphObjekt.h"
 
 
 class Line : public GraphObjekt
 {
-
+private:
+    QPoint endPoint;
 
 public:
     Line();
     ~Line();
+    QPoint getEndPoint();
+    QPoint setEndPoint();
 };
 
 #endif // LINE_H
